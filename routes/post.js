d399160e722cbf9d9498b117f52769b2019303b9
Robert Abreu <robertabreu@pursuit.org>
2019-01-25 18:23:09 -0500

    added create and read functions for post services; GET post by id route functional

--- routes/post.js
+++ routes/post.js
@@ -1,13 +1,21 @@
 const express = require('express');
-
+const PostService = require('../services/post');
 const postPublic = express.Router();
 
 postPublic.get('/:post_id', (req, res) => {
-    const {post_id} = req.params;
 
-    res.json({
-        message: `GET public post, post_id: ${post_id}`,
-    })
+    // gets post by id
+    const {post_id} = req.params;
+    PostService.readByID(post_id)
+    .then( data => {
+        res.json({
+            data,
+        });
+        return;
+    }).catch( err => {
+        res.json ({message: 'error, no post found',})
+        return;
+    });
 });
 
 postPublic.get('/:post_id/comments', (req, res) => {
--- services/post.js
+++ services/post.js
@@ -1,1 +1,24 @@
 const  db = require('./pgp');
+
+const create = (author, title, body) => {
+    return db.none(`INSERT INTO posts (author, title, body)
+    VALUES ($[author], $[title], $[body]);` , {author, title, body});
+};
+
+
+// returns everything about post, may want to omit author id
+const readByID = (id) => {
+    return db.any(`SELECT author, title, body FROM posts 
+    WHERE id = $[id];`, {id});
+};
+
+const readByAuthor = (author) => {
+    return db.any(`SELECT author, title, body FROM posts 
+    WHERE author = $[author];`, {author});
+};
+
+module.exports = {
+    create,
+    readByID, 
+    readByAuthor,
+};
